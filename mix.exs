c54e75b9d188b72150c340ebf5c028d48a9bae92
drinS <ha.sooofi@gmail.com>
2026-08-18 10:16:08 +0700

    feat: add bcryptrs dependency for OJS bcrypt password hashes

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule OjsLanding.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:ecto_sql, "~> 3.13"},
       {:postgrex, ">= 0.0.0"},
+      {:bcryptrs, "~> 0.1.4"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.2.0"},
       {:lazy_html, ">= 0.1.0", only: :test},
